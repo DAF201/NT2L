6019f5703333a582c056459bc7b4cf19a5e383cf
fangzhou ye <fangzhou.ye@fii-na.com>
2025-06-25 18:27:25 -0700

    test

--- setup.py
+++ setup.py
@@ -3,27 +3,27 @@ import setuptools
 
 
 setuptools.setup(
-    name='NT2L',
-    version='2.0.1',
-    description='NEIS_TOOLS_2 Lite',
-    author='NEI',
-    author_email='daf201@blink-in.com',
+    name="nt2l",
+    version="2.0.1",
+    description="NEIS_TOOLS_2 Lite",
+    author="NEI",
+    author_email="daf201@blink-in.com",
     install_requires=[
-        'pywin32',
-        'beautifulsoup4',
-        'requests',
-        'psutil',
+        "pywin32",
+        "beautifulsoup4",
+        "requests",
+        "psutil",
     ],
     packages=[
-        'nt2',
-        'core',
-        'config',
-        'export',
-        'service',
-        'functions',],
+        "nt2",
+        "core",
+        "config",
+        "export",
+        "service",
+        "functions",],
     entry_points={
-        'console_scripts': [
-            'nt2=nt2.main:main',
+        "console_scripts": [
+            "nt2=nt2.main:main",
         ],
     },
     python_requires=">=3.11",
